5b9138d21aed01f5a1be08bb2857f6743ee11410
jeremy <jeremy.lancereau@ensea.fr>
2023-12-06 17:19:51 +0100

    main mult et add

--- p1.cu
+++ p1.cu
@@ -2,9 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
+
+
 void MatrixInit(float *M, int n, int p){
     
+    
     for(int i=0; i<n; i++){
+
         for(int j=0; j<p;j++){
             M[i*p+j] = ((float)rand() / RAND_MAX)*2.0f - 1.0f;
         }
@@ -44,8 +48,8 @@ void MatrixMult(float *M1, float *M2, float *Mout, int n) {
 }
 
 __global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int i = blockIdx.x;
+    int j = threadIdx.x;
 
     if (i < n && j < p) {
         Mout[i * p + j] = M1[i * p + j] + M2[i * p + j];
@@ -53,8 +57,8 @@ __global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p) {
 }
 
 __global__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n) {
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.x;
+    int col = threadIdx.x;
 
     if (row < n && col < n) {
         float sum = 0.0f;
@@ -68,8 +72,8 @@ __global__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n) {
 
 
 // int main() {
-//     int n = 3;
-//     int p = 4;
+//     int n = 4;
+//     int p = 8;
 //     float *matrix;
 //     float *d_matrix; // Pointeur pour la matrice sur le GPU
 
@@ -94,87 +98,163 @@ __global__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n) {
 
 // }
 
+/// tester  la somme et la multiplication de matrices  
+
+int main() {
+    int n = 4;
+    int p = 4;
+    float *matrix;
+    float *d_matrix; // Pointeur pour la matrice sur le GPU
+
+    float *matrix2;
+    float *d_matrix2;
+
+    float *matrixOut;
+    float *d_matrixOut;
+
+    float *matrixOutMult;
+    float *d_matrixOutMult;
+    srand(time(NULL));
+
+    // Allocation de mémoire pour la matrice sur le GPU
+    cudaMalloc((void **)&d_matrix, n * p * sizeof(float));
+    cudaMalloc((void **)&d_matrix2, n * p * sizeof(float));
+    cudaMalloc((void **)&d_matrixOut, n * p * sizeof(float));
+    cudaMalloc((void **)&d_matrixOutMult, n * p * sizeof(float));
+
+    // Allocation et initialisation de la matrice sur le CPU
+    matrix = (float *)malloc(n * p * sizeof(float));
+    matrix2 = (float *)malloc(n * p * sizeof(float));
+    matrixOut = (float *)malloc(n * p * sizeof(float));
+    matrixOutMult = (float *)malloc(n * p * sizeof(float));
+
+    MatrixInit(matrix, n, p);
+    MatrixInit(matrix2, n, p);
+
+    // Copie de la matrice du CPU vers le GPU
+    cudaMemcpy(d_matrix, matrix, n * p * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_matrix2, matrix2, n * p * sizeof(float), cudaMemcpyHostToDevice);
+    //dim3 gridDim(n,p);
+
+    cudaMatrixAdd<<<n,p>>>(d_matrix, d_matrix2, d_matrixOut, n, p);
+    cudaMatrixMult<<<n,p>>>(d_matrix, d_matrix2, d_matrixOutMult, n);
+
+    cudaMemcpy(matrixOut, d_matrixOut, n * p * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(matrixOutMult, d_matrixOutMult, n * p * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Affichage de la matrice sur le CPU
+    printf("Matrice 1 :\n");
+
+    MatrixPrint(matrix, n, p);
+
+    printf("Matrice 2 :\n");
+
+    MatrixPrint(matrix2, n, p);
+
+    printf("Somme :\n");
+
+    MatrixPrint(matrixOut, n, p);
+
+    printf("Mult :\n");
+
+    MatrixPrint(matrixOutMult, n, p);
+    // Libération de la mémoire sur le CPU et le GPU
+    free(matrix);
+    cudaFree(d_matrix);
+
+    free(matrix2);
+    cudaFree(d_matrix2);
+
+    free(matrixOut);
+    cudaFree(d_matrixOut);
+
+    free(matrixOutMult);
+    cudaFree(d_matrixOutMult);
+
+    return 0;
+
+}
 
 /// TESTER LE TEMPS NECESSAIRE POUR LA SOMME ET LA MULTIPLICATION DE DEUX MATRICES AVEC ET SANS GPU
 
-int main() {
-    int n = 1000; // Taille des matrices (n x n)
-    int p = 500;  // Nombre de colonnes pour les matrices
+// int main() {
+//     int n = 1000; // Taille des matrices (n x n)
+//     int p = 500;  // Nombre de colonnes pour les matrices
 
-    // Allocation de mémoire pour les matrices sur le CPU
-    float *h_matrix1, *h_matrix2, *h_result_cpu_add, *h_result_cpu_mult;
-    h_matrix1 = (float *)malloc(n * p * sizeof(float));
-    h_matrix2 = (float *)malloc(n * p * sizeof(float));
-    h_result_cpu_add = (float *)malloc(n * p * sizeof(float));
-    h_result_cpu_mult = (float *)malloc(n * n * sizeof(float));
+//     // Allocation de mémoire pour les matrices sur le CPU
+//     float *h_matrix1, *h_matrix2, *h_result_cpu_add, *h_result_cpu_mult;
+//     h_matrix1 = (float *)malloc(n * p * sizeof(float));
+//     h_matrix2 = (float *)malloc(n * p * sizeof(float));
+//     h_result_cpu_add = (float *)malloc(n * p * sizeof(float));
+//     h_result_cpu_mult = (float *)malloc(n * n * sizeof(float));
 
-    // Initialisation des matrices sur le CPU (ajustez selon votre méthode d'initialisation)
-    // ...
+//     // Initialisation des matrices sur le CPU (ajustez selon votre méthode d'initialisation)
+//     // ...
 
-    // Allocation de mémoire sur le GPU
-    float *d_matrix1, *d_matrix2, *d_result_gpu_add, *d_result_gpu_mult;
-    cudaMalloc((void **)&d_matrix1, n * p * sizeof(float));
-    cudaMalloc((void **)&d_matrix2, n * p * sizeof(float));
-    cudaMalloc((void **)&d_result_gpu_add, n * p * sizeof(float));
-    cudaMalloc((void **)&d_result_gpu_mult, n * n * sizeof(float));
+//     // Allocation de mémoire sur le GPU
+//     float *d_matrix1, *d_matrix2, *d_result_gpu_add, *d_result_gpu_mult;
+//     cudaMalloc((void **)&d_matrix1, n * p * sizeof(float));
+//     cudaMalloc((void **)&d_matrix2, n * p * sizeof(float));
+//     cudaMalloc((void **)&d_result_gpu_add, n * p * sizeof(float));
+//     cudaMalloc((void **)&d_result_gpu_mult, n * n * sizeof(float));
 
-    // Copie des données du CPU vers le GPU
-    cudaMemcpy(d_matrix1, h_matrix1, n * p * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_matrix2, h_matrix2, n * p * sizeof(float), cudaMemcpyHostToDevice);
+//     // Copie des données du CPU vers le GPU
+//     cudaMemcpy(d_matrix1, h_matrix1, n * p * sizeof(float), cudaMemcpyHostToDevice);
+//     cudaMemcpy(d_matrix2, h_matrix2, n * p * sizeof(float), cudaMemcpyHostToDevice);
 
-    // Configuration des dimensions de la grille et des blocs pour l'addition sur GPU
-    dim3 blockDim_add(16, 16); // Par exemple, 16x16 threads par bloc
-    dim3 gridDim_add((n + blockDim_add.x - 1) / blockDim_add.x, (p + blockDim_add.y - 1) / blockDim_add.y);
+//     // Configuration des dimensions de la grille et des blocs pour l'addition sur GPU
+//     dim3 blockDim_add(16, 16); // Par exemple, 16x16 threads par bloc
+//     dim3 gridDim_add((n + blockDim_add.x - 1) / blockDim_add.x, (p + blockDim_add.y - 1) / blockDim_add.y);
 
-    // Mesure du temps pour la somme sur CPU
-    clock_t start_cpu_add = clock();
-    MatrixAdd(h_matrix1, h_matrix2, h_result_cpu_add, n, p);
-    clock_t end_cpu_add = clock();
-    double cpu_time_add = ((double)(end_cpu_add - start_cpu_add)) / CLOCKS_PER_SEC;
+//     // Mesure du temps pour la somme sur CPU
+//     clock_t start_cpu_add = clock();
+//     MatrixAdd(h_matrix1, h_matrix2, h_result_cpu_add, n, p);
+//     clock_t end_cpu_add = clock();
+//     double cpu_time_add = ((double)(end_cpu_add - start_cpu_add)) / CLOCKS_PER_SEC;
 
-    // Mesure du temps pour la somme sur GPU
-    clock_t start_gpu_add = clock();
-    cudaMatrixAdd<<<gridDim_add, blockDim_add>>>(d_matrix1, d_matrix2, d_result_gpu_add, n, p);
-    cudaDeviceSynchronize(); // Attente de la fin du kernel
-    clock_t end_gpu_add = clock();
-    double gpu_time_add = ((double)(end_gpu_add - start_gpu_add)) / CLOCKS_PER_SEC;
+//     // Mesure du temps pour la somme sur GPU
+//     clock_t start_gpu_add = clock();
+//     cudaMatrixAdd<<<gridDim_add, blockDim_add>>>(d_matrix1, d_matrix2, d_result_gpu_add, n, p);
+//     cudaDeviceSynchronize(); // Attente de la fin du kernel
+//     clock_t end_gpu_add = clock();
+//     double gpu_time_add = ((double)(end_gpu_add - start_gpu_add)) / CLOCKS_PER_SEC;
 
-    // Configuration des dimensions de la grille et des blocs pour la multiplication sur GPU
-    dim3 blockDim_mult(16, 16); // Par exemple, 16x16 threads par bloc
-    dim3 gridDim_mult((n + blockDim_mult.x - 1) / blockDim_mult.x, (n + blockDim_mult.y - 1) / blockDim_mult.y);
+//     // Configuration des dimensions de la grille et des blocs pour la multiplication sur GPU
+//     dim3 blockDim_mult(16, 16); // Par exemple, 16x16 threads par bloc
+//     dim3 gridDim_mult((n + blockDim_mult.x - 1) / blockDim_mult.x, (n + blockDim_mult.y - 1) / blockDim_mult.y);
 
-    // Mesure du temps pour la multiplication sur CPU
-    clock_t start_cpu_mult = clock();
-    MatrixMult(h_matrix1, h_matrix2, h_result_cpu_mult, n);
-    clock_t end_cpu_mult = clock();
-    double cpu_time_mult = ((double)(end_cpu_mult - start_cpu_mult)) / CLOCKS_PER_SEC;
+//     // Mesure du temps pour la multiplication sur CPU
+//     clock_t start_cpu_mult = clock();
+//     MatrixMult(h_matrix1, h_matrix2, h_result_cpu_mult, n);
+//     clock_t end_cpu_mult = clock();
+//     double cpu_time_mult = ((double)(end_cpu_mult - start_cpu_mult)) / CLOCKS_PER_SEC;
 
-    // Mesure du temps pour la multiplication sur GPU
-    clock_t start_gpu_mult = clock();
-    cudaMatrixMult<<<gridDim_mult, blockDim_mult>>>(d_matrix1, d_matrix2, d_result_gpu_mult, n);
-    cudaDeviceSynchronize(); // Attente de la fin du kernel
-    clock_t end_gpu_mult = clock();
-    double gpu_time_mult = ((double)(end_gpu_mult - start_gpu_mult)) / CLOCKS_PER_SEC;
+//     // Mesure du temps pour la multiplication sur GPU
+//     clock_t start_gpu_mult = clock();
+//     cudaMatrixMult<<<gridDim_mult, blockDim_mult>>>(d_matrix1, d_matrix2, d_result_gpu_mult, n);
+//     cudaDeviceSynchronize(); // Attente de la fin du kernel
+//     clock_t end_gpu_mult = clock();
+//     double gpu_time_mult = ((double)(end_gpu_mult - start_gpu_mult)) / CLOCKS_PER_SEC;
 
-    // Affichage des temps pour chaque opération
-    printf("Temps pour la somme de matrices sur CPU : %.5f secondes\n", cpu_time_add);
-    printf("Temps pour la somme de matrices sur GPU : %.5f secondes\n", gpu_time_add);
-    printf("Temps pour la multiplication de matrices sur CPU : %.5f secondes\n", cpu_time_mult);
-    printf("Temps pour la multiplication de matrices sur GPU : %.5f secondes\n", gpu_time_mult);
+//     // Affichage des temps pour chaque opération
+//     printf("Temps pour la somme de matrices sur CPU : %.5f secondes\n", cpu_time_add);
+//     printf("Temps pour la somme de matrices sur GPU : %.5f secondes\n", gpu_time_add);
+//     printf("Temps pour la multiplication de matrices sur CPU : %.5f secondes\n", cpu_time_mult);
+//     printf("Temps pour la multiplication de matrices sur GPU : %.5f secondes\n", gpu_time_mult);
 
-    // Libération de la mémoire sur le CPU et le GPU
-    free(h_matrix1);
-    free(h_matrix2);
-    free(h_result_cpu_add);
-    free(h_result_cpu_mult);
+//     // Libération de la mémoire sur le CPU et le GPU
+//     free(h_matrix1);
+//     free(h_matrix2);
+//     free(h_result_cpu_add);
+//     free(h_result_cpu_mult);
 
-    cudaFree(d_matrix1);
-    cudaFree(d_matrix2);
-    cudaFree(d_result_gpu_add);
-    cudaFree(d_result_gpu_mult);
+//     cudaFree(d_matrix1);
+//     cudaFree(d_matrix2);
+//     cudaFree(d_result_gpu_add);
+//     cudaFree(d_result_gpu_mult);
 
-    return 0;
-}
+//     return 0;
+// }
 
 /// TESTER LA PUISSANCE DU GPU ///
 
